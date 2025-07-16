7f6b1f536523a9badf16247b7ad01b67492d04e5
junyong <rlawnsdyd530@gmail.com>
2025-07-16 18:01:09 +0900

    first commit

---
+++ src/main/java/jy/toy/Toy.java
@@ -0,0 +1,7 @@
+package jy.toy;
+
+public class Toy {
+
+
+
+}
---
+++ src/main/java/jy/toy/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package jy.toy.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI toyAPI() {
+
+        Info info = new Info()
+                .title("장난감 API")
+                .description("나만의 장난감")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+
+        Components components = new Components();
+
+//        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+//
+//        Components components = new Components()
+//                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+//                        .name(jwtSchemeName)
+//                        .type(SecurityScheme.Type.HTTP)
+//                        .scheme("bearer")
+//                        .bearerFormat("JWT"));
+
+
+        OpenAPI openAPI = new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url("http://localhost:8080").description("local server"))
+                .components(components);
+
+        openAPI.addExtension("x-swagger-ui-disable-cache", true);
+        return openAPI;
+    }
+
+}
