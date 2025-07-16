d5a07f351c14bc712990d59aed826ad60326bde2
junyong <rlawnsdyd530@gmail.com>
2025-07-16 18:30:25 +0900

    feat : 구글 코드 적용

--- src/main/java/jy/toy/Toy.java
+++ src/main/java/jy/toy/Toy.java
@@ -2,11 +2,11 @@ package jy.toy;
 
 public class Toy {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        int yes = 1;
-        if (yes == 1) {
-            yes = 1;
-        }
+    int yes = 1;
+    if (yes == 1) {
+      yes = 1;
     }
+  }
 }
